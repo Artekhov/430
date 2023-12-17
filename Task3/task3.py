09e9dee7f7a5883976e7587fa407b7423ac87f7e
Artekhov <153942927+Artekhov@users.noreply.github.com>
2023-12-17 15:30:43 +0300

    Add files via upload

---
+++ Task3/task3.py
@@ -0,0 +1,23 @@
+import json
+
+# Чтение данных из файлов
+with open('tests.json', 'r') as tests_file:
+	tests_data = json.load(tests_file)
+
+with open('values.json', 'r') as values_file:
+	values_data = json.load(values_file)
+
+# Функция для заполнения значений тестов
+def fill_test_values(test, values):
+	if 'id' in test and test['id'] in values:
+		test['value'] = values[test['id']]
+		if 'values' in test:
+			for subtest in test['values']:
+				fill_test_values(subtest, values)
+
+# Заполнение значений тестов
+fill_test_values(tests_data, values_data)
+
+# Запись данных в файл report.json
+with open('report.json', 'w') as report_file:
+	json.dump(tests_data, report_file, indent=4)
