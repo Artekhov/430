98cd24744da73f124c6ebca0a5e4194a2cddbc17
Artekhov <153942927+Artekhov@users.noreply.github.com>
2023-12-17 15:35:09 +0300

    Add files via upload

---
+++ Task4/task4.py
@@ -0,0 +1,7 @@
+import sys
+for i in range(1,len(sys.argv)): 
+    with open(file = sys.argv[i], mode = "r", encoding = "utf-8") as f:
+        list=f.readlines()
+    list=[int (x) for x in list]  
+    med = sorted(list)[len(list) // 2]  # медиана списка
+    print(sum(abs(j-med) for j in list))
